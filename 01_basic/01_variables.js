6f386dffd877450a5408b24c396f275d72995d5c
Vishal200728 <godedit178@gmail.com>
2025-11-07 10:47:27 +0530

    Create 01_variables.js

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 98765
+let accountemail = "vishal123@google.com"
+var accountpassword = "12345"
+acoountcity = "dharamshala"
+
+console.log(accountID)
+console.table([accountID,accountemail,accountpassword,acoountcity])
